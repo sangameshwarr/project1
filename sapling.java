538bc5fd2eea43e9a8b6ef265b49228c10347684
sangameshwarr <127983968+sangameshwarr@users.noreply.github.com>
2023-03-16 13:20:35 +0530

    Add files via upload

---
+++ sapling.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class sapling {
+    public static void main(String[] args){
+        JFrame frame=new JFrame("sapling project");
+        frame.setBounds(200,200,500,500);
+        frame.getContentPane().setBackground(Color.red);
+        frame.setVisible(true);
+    }
+}
